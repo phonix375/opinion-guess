d89c030853f67d3118858688cd8297b94834dab8
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-31 17:04:12 -0400

    added login email and password validation

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { User } = require('../../models');
 
 
@@ -6,6 +7,32 @@ router.get('/', (req, res) => {
     res.json({message:'this is working !'})
   });
 
-
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+      
+      const validPassword = bcrypt.compareSync(req.body.password,dbUserData.password);
   
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+  });
 module.exports = router;
--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -6,6 +6,23 @@ function newGameHandler(event){
     alert('you clicked the new game btn');
 }
 
+async function submitLoginForm(event){
+    event.preventDefault();
+
+    const email = document.querySelector('#email').value.trim();
+    
+    const password = document.querySelector('#password').value.trim();
+    if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))){
+        alert('please enter a valid Email address');
+        return;
+    }
+    if(!password){
+        alert('Please enter a password')
+    }
+    console.log(email,password);
+}
+
 
 document.querySelector('#joinGame').addEventListener('click', joinGameHandler);
 document.querySelector('#newGame').addEventListener('click', newGameHandler);
+document.querySelector('#loginSubmit').addEventListener('click', submitLoginForm);
