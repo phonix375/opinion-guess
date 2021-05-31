6ca7155dec21190c71d6e37726731d50940c9d2e
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-31 01:46:59 -0400

    create a main page and login modal

---
+++ controllers/home-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+router.get('/', (req,res) =>{
+    res.render('homepage', {loggedIn: req.session.loggedIn})
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({force:true}).then(()=>{
