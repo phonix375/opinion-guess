f54374bd7e2876a83432573afa594c378ca1c561
Alexy Kotliar <kotliar.alex@gmail.com>
2021-06-06 15:03:35 -0400

    fix bug

--- public/javascript/play.js
+++ public/javascript/play.js
@@ -112,24 +112,26 @@ document.querySelector('#addNickName').addEventListener('click', function(event)
  
 })
 
-// Modal for HighScores
-var myModal = new bootstrap.Modal(document.getElementById("HighScores"), {});
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+// // Modal for HighScores
+// var myModal2 = new bootstrap.Modal(document.getElementById("HighScores"), {});
+// var modal = document.getElementById("myModal");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+
+var myModal = new bootstrap.Modal(document.getElementById("nickName"), {});
